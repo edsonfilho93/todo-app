6ba867b1d72119fd13513110d87e394f1a1fdb86
Edson <edsonfilho734@gmail.com>
2020-08-05 20:24:57 -0300

    Finalização do backend

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
-module.exports = mongoose.connect('mongodb://localhost:17017/test')
-console.log('Mongodb OK')
+module.exports = mongoose.connect('mongodb://localhost:17017/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
